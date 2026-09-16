1cae1ca57bf8f906556dd80d2cbe87483024bbe8
myrceapaul <myrceapaul@yahoo.com>
2026-09-16 21:26:26 +0100

    Add functions for customer and system alerts

---
+++ app.js
@@ -0,0 +1,19 @@
+function showNewCustomer() {
+    alert("New Customer");
+}
+
+function showSearch() {
+    alert("Search Customer");
+}
+
+function showHistory() {
+    alert("Consultation History");
+}
+
+function showPatchTests() {
+    alert("Patch Tests");
+}
+
+function showBackup() {
+    alert("Backup / Restore");
+}
